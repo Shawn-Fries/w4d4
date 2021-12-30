69d7f163dc6b2ffd188341114506217d786afcc9
avakil3 <aagamvakil@hotmail.com>
2021-12-30 14:08:02 -0500

    finished largest contiguous_subsum

--- execution_time.rb
+++ execution_time.rb
@@ -1,25 +1,64 @@
-def my_min(arr)
-    min = arr.first
+def my_min(arr) # O(n^2)
+   # min = arr.first
     arr.each_with_index do |ele_1, i|
-       # p min
+       comparisons = []
         arr.each_with_index do |ele_2, j|
-            if ele_2 < min && j != i
-                min = ele_2
-                break
+            if  j != i
+                comparisons <<  ( ele_1 < ele_2 )
             end
         end
+        puts comparisons
+        return ele_1 if comparisons.all?(true) 
     end
 
     min
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+ # my_min(list)  # =>  -5
 
-def my_min_2(arr)
+def my_min_2(arr) # O(n)
     min = arr.first
     arr.each {|ele| min = ele if ele <= min}
     min
 end
 
-p my_min_2(list)  # =>  -5
+# p my_min_2(list)  # =>  -5
+
+
+def largest_contiguous_subsum(array) #O(n^2)
+
+    subArrays = []
+
+    (0...array.length).each do |i| #O(n^2)
+        (i...array.length).each do |j|
+            subArrays << array[i..j]
+        end
+    end
+   max = array.first
+   subArrays.each do |subArr| #O(n)
+        max = subArr.sum if subArr.sum > max
+    end
+
+    max
+    
+end
+
+# p largest_contiguous_subsum([5, 3, -7])
+
+
+
+def largest_contiguous_subsum_2(array)
+    current_sum = array.first
+    largest_sum = array.first
+    (1...array.length).each do |i|
+        current_sum = 0 if current_sum < 0
+        current_sum += array[i] # 5
+        largest_sum = current_sum if current_sum >= largest_sum #largest_sum = 5    current_sum  = 5 
+        
+    end
+    largest_sum
+
+end
+
+p largest_contiguous_subsum_2([2, 3, -6, 7, -6, 7])
